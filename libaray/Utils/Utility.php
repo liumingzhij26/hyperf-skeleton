7cdd7c1a13e49c3287d3b148aa3b4aec4fa36b5f
mingzhi.L <liumingzhij26@gmail.com>
2020-05-20 10:21:02 +0800

    feat: 配置文档

---
+++ libaray/Utils/Utility.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+if (!function_exists('demo')) {
+
+    /**
+     * demo
+     *
+     * @param $data
+     * @return mixed
+     */
+    function demo($data)
+    {
+        return $data;
+    }
+}
