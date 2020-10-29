fc8fff93921192bd0f84ff94bb873502af0f24fe
liumingzhi <liumingzhij26@qq.com>
2020-10-29 17:27:38 +0800

    feat: 升级

--- config/autoload/server.php
+++ config/autoload/server.php
@@ -32,7 +32,7 @@ return [
                 'name' => 'json-rpc',
                 'type' => Server::SERVER_BASE,
                 'host' => '0.0.0.0',
-                'port' => (int) env('RPC_POST', 3201),
+                'port' => (int) env('RPC_PORT', 3201),
                 'sock_type' => SWOOLE_SOCK_TCP,
                 'callbacks' => [
                     SwooleEvent::ON_RECEIVE => [TcpServer::class, 'onReceive'],
@@ -52,7 +52,7 @@ return [
                 'name' => 'http',
                 'type' => Server::SERVER_HTTP,
                 'host' => '0.0.0.0',
-                'port' => (int) env('HTTP_POST', 4201),
+                'port' => (int) env('HTTP_PORT', 4201),
                 'sock_type' => SWOOLE_SOCK_TCP,
                 'callbacks' => [
                     SwooleEvent::ON_REQUEST => [HttpServer::class, 'onRequest'],
