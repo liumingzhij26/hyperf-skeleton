46aff9d813ae54bcb76294495e9960e9e2c35ea7
liumingzhi <liumingzhij26@qq.com>
2021-01-19 23:01:10 +0800

    feat: 升级2.1

---
+++ bin/stop.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env sh
+
+basepath=$(cd `dirname $0`; pwd)
+cd $basepath
+cd ../
+
+ app_path=$(pwd)
+
+if [ ! -f "composer.lock" ]; then
+  echo "Not found composer.lock, please composer install first."
+  exit
+fi
+echo "[start] git pull"
+git pull
+echo "[start] init cache"
+
+# 配置环境变量，这一步非常重要
+export COMPOSER_HOME=$app_path
+# systemctl 里面没有找具体的项目目录，必须指定路径
+/usr/local/bin/composer dump-autoload -o -d $app_path -q
+
+echo "[start] init cache success"
+
+echo "[start] unit test start..."
+if ! /usr/local/bin/composer test -d $app_path -q; then
+    echo "unit test fail..."
+    echo "unit test fail..."
+    echo "unit test fail..."
+    exit
+fi
+echo "[start] unit test success..."
+
+echo "[start] service stop"
+
+if [ -f "./runtime/hyperf.pid" ]; then
+    pid=$(cat ./runtime/hyperf.pid)
+    echo $pid
+    /bin/kill -USR1 $pid
+fi
